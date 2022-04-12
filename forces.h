35ed86e69d3e744087edf70f0dc177c1fb6bfe84
Paul Delifer <55153194+Zall9@users.noreply.github.com>
2022-04-12 14:40:08 +0200

    comi
    commit

--- forces.h
+++ forces.h
@@ -2,21 +2,27 @@
 #define _FORCES_H_
 
 #include "particules.h"
-
-#define NB_FORCES 1
+//#include "obstacles.h"
+#define NB_FORCES 2
 /// Un seul type de force pour le moment.
-typedef enum { GRAVITE }  ForceType;
+typedef enum
+{
+  GRAVITE,
+  GRAVITATION_UNIV
+} ForceType;
 
 /// Une force est un type et des paramètres qui la définissent.
-struct SForce {
+struct SForce
+{
   ForceType type;
-  double params[ 2 ];
+  double params[2];
 };
 typedef struct SForce Force;
 
 /// Définit la force de gravité dans la direction donnée.
-Force gravite( double gx, double gy );
+Force gravitationUniv(double gx, double gy);
+Force gravite(double gx, double gy);
 /// Ajoute à la particule \a p la force donnée \a f
-void appliqueForce( Particule* p, Force* f );
+void appliqueForce(Particule *p, Force *f);
 
 #endif
